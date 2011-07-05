ad7c674aaa0211af24b5dcf24d682fb573a14c80
Ronald Ashri <ronald@istos.it>
2011-07-05 14:26:16 +0200

    Some changes mostly for test purposes
    Signed-off-by: Ronald Ashri <ronald@istos.it>

--- modules/rooms_availability/rooms_availability.tpl.php
+++ modules/rooms_availability/rooms_availability.tpl.php
@@ -1,4 +1,4 @@
-<?php //dpm($variables) ?>
+<?php dpm($variables) ?>
 
 <h1 style="text-transform:capitalize"><?php print $type ?> - <?php print $name ?> Availability View </h1>
 
@@ -9,3 +9,14 @@
 
 
 <div id='calendar2' style="float:left;width:400px;margin:20px 10px;"></div>
+
+<div style="clear:both"></div>
+
+
+<h2>Update Room Availability</h2>
+
+<p>Careful this will overwrite any existing bookings.</p>
+
+
+
+<?php print render($variables['update_form']); ?>
