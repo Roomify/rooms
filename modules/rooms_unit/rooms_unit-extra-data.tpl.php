09eb5b99472170e79fd3e03d63e8d77ce2efe248
Ronald Ashri <ronald@istos.it>
2011-07-11 13:13:12 +0200

    I love the smell of a huge refactor in the morning - gone are rooms_room and welcome to rooms_unit - a saner way to think about booking units (+ lots of clean up of code)
    Signed-off-by: Ronald Ashri <ronald@istos.it>

---
+++ modules/rooms_unit/rooms_unit-extra-data.tpl.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * Example tpl file for theming a single unit-specific theme
+ *
+ * Available variables:
+ * - $status: The variable to theme (while only show if you tick status)
+ * 
+ * Helper variables:
+ * - $rooms_room: The Room object the variables are derived from
+ */
+?>
+
+<div class="rooms_unit-status">
+  <?php print '<strong>Unit Sample Data:</strong> ' . $rooms_unit_sample_data = ($rooms_unit_sample_data) ? 'Switch On' : 'Switch Off' ?>
+</div>
