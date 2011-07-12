195e2d167b96fe556b4265d1e9f0e8ed6c721af6
Ronald Ashri <ronald@istos.it>
2011-07-12 08:51:00 +0200

    Improvements to the date availability management screen
    Signed-off-by: Ronald Ashri <ronald@istos.it>

--- modules/rooms_availability/rooms_availability.tpl.php
+++ modules/rooms_availability/rooms_availability.tpl.php
@@ -1,21 +1,32 @@
 <div class="<?php print $variables['classes']?>">
 <h1 style="text-transform:capitalize"><?php print $type ?> - <?php print $name ?> Availability View </h1>
 
-<div id='date-backward'>
-  <?php print $backward_link ?>
-</div>
-<div id='date-forward'>
-  <?php print $forward_link ?>
-</div>
+<table>
+  <tr>
+    <td style="width:33%;text-align:left;">
+        <?php print $backward_link ?>
+    </td>
+    <td style="width:33%;text-align:center">
+        <?php print $current_link ?>      
+    </td>
+    <td style="width:33%;text-align:right">
+      <?php print $forward_link ?>
+    </td>
+  </tr>
+  <tr>
+    <td><div id='calendar' class="month1"></div></td>
+    <td><div id='calendar1' class="month2"></div></td>
+    <td><div id='calendar2' class="month3"></div></td>
+  </tr>
+
+</table>
+
 <div style="clear:both"></div>
 
-<div id='calendar' class="month1"></div>
 
 
-<div id='calendar1' class="month2"></div>
 
 
-<div id='calendar2' class="month3"></div>
 
 <div style="clear:both"></div>
 
