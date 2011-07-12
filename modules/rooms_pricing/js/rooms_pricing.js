e607dd4f059d3f6f3a93e99d7fc7acb11ee471f9
Ronald Ashri <ronald@istos.it>
2011-07-12 12:41:54 +0200

    Added pricing management and a fix for handling multiple unit creation
    Signed-off-by: Ronald Ashri <ronald@istos.it>

---
+++ modules/rooms_pricing/js/rooms_pricing.js
@@ -0,0 +1,71 @@
+(function ($) {
+
+Drupal.behaviors.rooms_availability = {
+  attach: function(context) {
+        
+    $currentmonth = parseInt(Drupal.settings.currentMonth);
+    $currentyear = parseInt(Drupal.settings.currentYear);
+    if ($currentmonth == 12) {
+      $nextmonth1 = 1;
+      $nextyear1 = parseInt(Drupal.settings.currentYear) + 1;
+      $nextmonth2 = 2;
+      $nextyear2 = parseInt(Drupal.settings.currentYear) + 1;
+    }
+    if ($currentmonth == 11){
+      $nextmonth1 = 12;
+      $nextyear1 = Drupal.settings.currentYear;
+      $nextmonth2 = 2;
+      $nextyear2 = parseInt(Drupal.settings.currentYear) + 1;
+    }
+    else{
+      $nextmonth1 = $currentmonth+1;
+      $nextyear1 = $currentyear;
+      $nextmonth2 = $currentmonth+2;
+      $nextyear2 = $currentyear;
+    }
+    
+    $('#calendar').fullCalendar({
+      editable:false,
+      month:$currentmonth-1,
+      year:Drupal.settings.currentYear,
+      header:{
+        left: 'title',
+        center: '',
+        right: ''
+      },
+      events: Drupal.settings.basePath + 'admin/rooms/units/unit/' + Drupal.settings.roomID + '/pricing/json/' + $currentyear + '/' + $currentmonth
+    });
+    
+    $('#calendar1').fullCalendar({
+      editable:false,
+      month:$nextmonth1-1,
+      year:Drupal.settings.currentYear,
+      header:{
+        left: 'title',
+        center: '',
+        right: ''
+      },
+      events: Drupal.settings.basePath + 'admin/rooms/units/unit/' + Drupal.settings.roomID + '/pricing/json/' + $nextyear1 + '/' + $nextmonth1
+    });
+
+    $('#calendar2').fullCalendar({
+      editable:false,
+      month:$nextmonth2-1,
+      year:Drupal.settings.currentYear,
+      header:{
+        left: 'title',
+        center: '',
+        right: ''
+      },
+      events: Drupal.settings.basePath + 'admin/rooms/units/unit/' + Drupal.settings.roomID + '/pricing/json/' + $nextyear2 + '/' + $nextmonth2
+    });
+
+    // Resize takes care of some quirks on occasion
+    $(window).resize();
+
+  }
+};
+})(jQuery);
+  
+  
+
--- modules/rooms_pricing/rooms_pricing.tpl.php
+++ modules/rooms_pricing/rooms_pricing.tpl.php
@@ -1,43 +1,40 @@
-<?php
+<div class="<?php print $variables['classes']?>">
+<h1 style="text-transform:capitalize"><?php print $type ?> - <?php print $name ?> Pricing View </h1>
 
-/**
- * @file
- * A basic template for room entities
- *
- * Available variables:
- * - $content: An array of comment items. Use render($content) to print them all, or
- *   print a subset such as render($content['field_example']). Use
- *   hide($content['field_example']) to temporarily suppress the printing of a
- *   given element.
- * - $title: The name of the room
- * - $url: The standard URL for viewing a room entity
- * - $page: TRUE if this is the main view page $url points too.
- * - $classes: String of classes that can be used to style contextually through
- *   CSS. It can be manipulated through the variable $classes_array from
- *   preprocess functions. By default the following classes are available, where
- *   the parts enclosed by {} are replaced by the appropriate values:
- *   - entity-profile
- *   - room-{TYPE}
- *
- * Other variables:
- * - $classes_array: Array of html class attribute values. It is flattened
- *   into a string within the variable $classes.
- *
- * @see template_preprocess()
- * @see template_preprocess_entity()
- * @see template_process()
- */
-?>
-<div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-  <?php if (!$page): ?>
-    <h2<?php print $title_attributes; ?>>
-        <a href="<?php print $url; ?>"><?php print $title; ?></a>
-    </h2>
-  <?php endif; ?>
+<table>
+  <tr>
+    <td style="width:33%;text-align:left;">
+        <?php print $backward_link ?>
+    </td>
+    <td style="width:33%;text-align:center">
+        <?php print $current_link ?>      
+    </td>
+    <td style="width:33%;text-align:right">
+      <?php print $forward_link ?>
+    </td>
+  </tr>
+  <tr>
+    <td><div id='calendar' class="month1"></div></td>
+    <td><div id='calendar1' class="month2"></div></td>
+    <td><div id='calendar2' class="month3"></div></td>
+  </tr>
 
-  <div class="content"<?php print $content_attributes; ?>>
-    <?php
-      print render($content);
-    ?>
-  </div>
+</table>
+
+<div style="clear:both"></div>
+
+
+
+
+
+
+<div style="clear:both"></div>
+
+
+<h2>Update Room Pricing</h2>
+
+<p>Careful this will overwrite any existing pricing info.</p>
+
+
+<?php print render($variables['update_form']); ?>
 </div>
