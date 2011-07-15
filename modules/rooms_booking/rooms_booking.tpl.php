5daaac5eaeb0c0cffc3795d6f6731f2e289ce72e
Ronald Ashri <ronald@istos.it>
2011-07-15 22:03:58 +0200

    Basis for booking search there
    Signed-off-by: Ronald Ashri <ronald@istos.it>

---
+++ modules/rooms_booking/rooms_booking.tpl.php
@@ -0,0 +1,14 @@
+<? dpm($variables) ?>
+
+<?php if (!$booking_results): ?>
+  <?php print render($no_results); ?>
+<?php endif; ?>
+
+<?php if ($booking_results): ?>
+  <?php foreach ($units as $unit) {
+    print render($unit['unit']);
+    print render($unit['book_unit_form']);
+  }?>
+<?php endif; ?>
+  
+  
