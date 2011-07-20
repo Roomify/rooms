1e918c39983456e1e46ad45d5a468817f7ee908d
Ronald Ashri <ronald@istos.it>
2011-07-21 01:04:04 +0200

    Showing the price in booking results page
    Signed-off-by: Ronald Ashri <ronald@istos.it>

--- modules/rooms_booking/rooms_booking.tpl.php
+++ modules/rooms_booking/rooms_booking.tpl.php
@@ -7,6 +7,7 @@
 <?php if ($booking_results): ?>
   <?php foreach ($units as $unit_id => $unit) {
     print render($unit['unit']);
+    print render($unit['price']);
     print render($unit['book_unit_form']);
   }?>
 <?php endif; ?>
