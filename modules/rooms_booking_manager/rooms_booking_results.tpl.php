723cc2cd8132f5ad344a5efa2aa3f15f9cd01f0a
Ronald Ashri <ronald@istos.it>
2011-07-26 02:05:26 +0200

    .tpl handles two view cases: - Individual will show you each unit on its own - By Type will show units arranged by type and price level
    Signed-off-by: Ronald Ashri <ronald@istos.it>

--- modules/rooms_booking_manager/rooms_booking_results.tpl.php
+++ modules/rooms_booking_manager/rooms_booking_results.tpl.php
@@ -1,14 +1,26 @@
+<?php dpm($variables) ?>
 <?php if (!$booking_results): ?>
   <?php print render($booking_search_form); ?>
   <?php print render($no_results); ?>
 <?php endif; ?>
 
-<?php if ($booking_results): ?>
-  <?php foreach ($units as $unit_id => $unit) {
-    print render($unit['unit']);
-    print render($unit['price']);
-    print render($unit['book_unit_form']);
-  }?>
+<?php if (isset($style) && ($style == 'individual')): ?>
+  <?php if ($booking_results): ?>
+    <?php foreach ($units_per_type as $unit_type => $units_per_price_level) {
+      print $unit_type;
+      foreach ($units_per_price_level as $price => $unit) {
+        print render($unit[key($unit)]['unit']);
+        print render($unit[key($unit)]['price']);
+        print render($unit[key($unit)]['book_unit_form']);
+      }
+    }?>
+  <?php endif; ?>
+<?php endif; ?>
+
+<?php if (isset($style) && ($style == 'per_type')): ?>
+  <?php if ($booking_results): ?>
+        <?php print render($units_per_type_form); ?>
+  <?php endif; ?>
 <?php endif; ?>
   
   
