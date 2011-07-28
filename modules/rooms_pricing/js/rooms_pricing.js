9ae682e684749b12c6a968cacf6d09973b8471ec
Ronald Ashri <ronald@istos.it>
2011-07-28 07:44:38 +0200

    Cleaned up the javascript that handles placing pricing information on FullCalendar to reduce code repetition
    Signed-off-by: Ronald Ashri <ronald@istos.it>

--- modules/rooms_pricing/js/rooms_pricing.js
+++ modules/rooms_pricing/js/rooms_pricing.js
@@ -3,66 +3,47 @@
 Drupal.behaviors.rooms_availability = {
   attach: function(context) {
         
-    $currentmonth = parseInt(Drupal.settings.currentMonth);
-    $currentyear = parseInt(Drupal.settings.currentYear);
-    if ($currentmonth == 12) {
-      $nextmonth1 = 1;
-      $nextyear1 = parseInt(Drupal.settings.currentYear) + 1;
-      $nextmonth2 = 2;
-      $nextyear2 = parseInt(Drupal.settings.currentYear) + 1;
+    currentmonth = parseInt(Drupal.settings.currentMonth);
+    currentyear = parseInt(Drupal.settings.currentYear);
+    if (currentmonth == 12) {
+      nextmonth1 = 1;
+      nextyear1 = parseInt(Drupal.settings.currentYear) + 1;
+      nextmonth2 = 2;
+      nextyear2 = parseInt(Drupal.settings.currentYear) + 1;
     }
-    if ($currentmonth == 11){
-      $nextmonth1 = 12;
-      $nextyear1 = Drupal.settings.currentYear;
-      $nextmonth2 = 2;
-      $nextyear2 = parseInt(Drupal.settings.currentYear) + 1;
+    if (currentmonth == 11){
+      nextmonth1 = 12;
+      nextyear1 = Drupal.settings.currentYear;
+      nextmonth2 = 2;
+      nextyear2 = parseInt(Drupal.settings.currentYear) + 1;
     }
     else{
-      $nextmonth1 = $currentmonth+1;
-      $nextyear1 = $currentyear;
-      $nextmonth2 = $currentmonth+2;
-      $nextyear2 = $currentyear;
+      nextmonth1 = currentmonth+1;
+      nextyear1 = currentyear;
+      nextmonth2 = currentmonth+2;
+      nextyear2 = currentyear;
     }
-    
-    $('#calendar').fullCalendar({
-      editable:false,
-      month:$currentmonth-1,
-      year:Drupal.settings.currentYear,
-      header:{
-        left: 'title',
-        center: '',
-        right: ''
-      },
-      events: Drupal.settings.basePath + 'admin/rooms/units/unit/' + Drupal.settings.roomID + '/pricing/json/' + $currentyear + '/' + $currentmonth
-    });
-    
-    $('#calendar1').fullCalendar({
-      editable:false,
-      month:$nextmonth1-1,
-      year:Drupal.settings.currentYear,
-      header:{
-        left: 'title',
-        center: '',
-        right: ''
-      },
-      events: Drupal.settings.basePath + 'admin/rooms/units/unit/' + Drupal.settings.roomID + '/pricing/json/' + $nextyear1 + '/' + $nextmonth1
-    });
 
-    $('#calendar2').fullCalendar({
-      editable:false,
-      month:$nextmonth2-1,
-      year:Drupal.settings.currentYear,
-      header:{
-        left: 'title',
-        center: '',
-        right: ''
-      },
-      events: Drupal.settings.basePath + 'admin/rooms/units/unit/' + Drupal.settings.roomID + '/pricing/json/' + $nextyear2 + '/' + $nextmonth2
-    });
-
+    var calendars = new Array();
+    calendars[0] = new Array('#calendar', currentmonth, currentyear);
+    calendars[1] = new Array('#calendar1', nextmonth1, nextyear1);
+    calendars[2] = new Array('#calendar2', nextmonth2, nextyear2);
+ 
+    $.each(calendars, function(key, value) {
+      $(value[0]).once().fullCalendar({
+        editable:false,
+        month:value[1]-1,
+        year:Drupal.settings.currentYear,
+        header:{
+          left: 'title',
+          center: '',
+          right: ''
+        },
+        events: Drupal.settings.basePath + 'admin/rooms/units/unit/' + Drupal.settings.roomID + '/pricing/json/' + value[2] + '/' + value[1]
+      });
+    });  
     // Resize takes care of some quirks on occasion
     $(window).resize();
-
   }
 };
 })(jQuery);
