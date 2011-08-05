afaca945f7ae0e5e50b1b3e70ea1c54755e59853
Ronald Ashri <ronald@istos.it>
2011-08-05 20:49:16 +0200

    Field allows you to embed availability information of a booking unit in any fieldable entity.
    Signed-off-by: Ronald Ashri <ronald@istos.it>

---
+++ modules/rooms_availability_reference/js/rooms_availability_reference.js
@@ -0,0 +1,43 @@
+(function ($) {
+
+Drupal.behaviors.rooms_availability_reference = {
+  attach: function(context) {    
+    var minDate =new Date(2011, 2, 1, 0, 0, 0, 0);
+    alert(Drupal.settings.roomsAvailabilityRef.unitID);
+    $('#calendar').fullCalendar({
+      ignoreTimezone:false,
+      editable:false,
+      month:'7',
+      year:'2011',
+      header:{
+        left: 'today',
+				center: 'title',
+				right: 'next'
+      },
+      events: Drupal.settings.basePath + '?q=admin/rooms/units/unit/'
+                                        + Drupal.settings.roomsAvailabilityRef.unitID
+                                        + '/availability/json/'
+                                        + Drupal.settings.roomsAvailabilityRef.startyear
+                                        + '/'
+                                        + Drupal.settings.roomsAvailabilityRef.startmonth
+                                        +'/1/'
+                                        + Drupal.settings.roomsAvailabilityRef.endyear
+                                        +'/'
+                                        + Drupal.settings.roomsAvailabilityRef.endmonth
+                                        +'/'
+                                        + Drupal.settings.roomsAvailabilityRef.endday
+                                        + '/'
+                                        + Drupal.settings.roomsAvailabilityRef.style
+    });
+    
+    
+    
+    // Resize takes care of some quirks on occasion
+    $(window).resize();
+
+  }
+};
+})(jQuery);
+  
+  
+
---
+++ modules/rooms_availability_reference/rooms_availability_field_calendar.tpl.php
@@ -0,0 +1,9 @@
+<?php dpm($variables); ?>
+
+<div class="<?php print $variables['classes']; ?>">
+<?php foreach ($rooms_availability_field_calendar['#calendar'] as $unit_calendar) { ?>
+  <?php print render($unit_calendar); ?>
+  <div id="calendar">
+  </div>  
+<?php } ?>
+</div>
