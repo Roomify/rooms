80571d72e43aa4e92bba8331e44624a2da063534
Nicolò Caruso <nicolo@istos.it>
2012-04-16 10:44:00 +0200

    Spelling fixes

--- modules/rooms_booking_manager/js/rooms_booking_manager_checked.js
+++ modules/rooms_booking_manager/js/rooms_booking_manager_checked.js
@@ -1,11 +1,8 @@
 (function ($) {
   $(document).ready(function() {
-    var clearMePrevious = '';
-    // clear input on focus
     $('#edit-perc-textfield').focus(function()
     {
         $('input[name=rooms_payment_options][value=11]').attr('checked', true);
     });
   });
 })(jQuery);
-;
