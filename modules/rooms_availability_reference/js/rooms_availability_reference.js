7ef2c8d937c484197105d939377f903cfb61eca4
Nicolò Caruso <nicolo@istos.it>
2012-12-17 16:17:44 +0100

    HCW-697 - Dynamic room labels on calendar

--- modules/rooms_availability_reference/js/rooms_availability_reference.js
+++ modules/rooms_availability_reference/js/rooms_availability_reference.js
@@ -1,36 +1,42 @@
 (function ($) {
 
+
 Drupal.behaviors.rooms_availability_reference = {
-  attach: function(context) {    
+  attach: function(context) {
     var minDate =new Date();
-    $('.cal').fullCalendar({
-      ignoreTimezone:false,
-      editable:false,
-      month:minDate.getMonth(),
-      year:minDate.getFullYear(),
-      header:{
-        left: 'today',
-				center: 'title',
-				right: 'prev, next'
-      },
-      events: Drupal.settings.basePath + '?q=rooms/units/unit/'
-                                        + Drupal.settings.roomsAvailabilityRef.unitID
-                                        + '/availability/json/'
-                                        + Drupal.settings.roomsAvailabilityRef.startyear
-                                        + '/'
-                                        + Drupal.settings.roomsAvailabilityRef.startmonth
-                                        +'/1/' //start day
-                                        + Drupal.settings.roomsAvailabilityRef.endyear
-                                        +'/'
-                                        + Drupal.settings.roomsAvailabilityRef.endmonth
-                                        +'/'
-                                        + Drupal.settings.roomsAvailabilityRef.endday
-                                        + '/'
-                                        + Drupal.settings.roomsAvailabilityRef.style
+    var i = 0;
+
+    $('.cal').each(function() {
+      $(this).fullCalendar({
+        ignoreTimezone:false,
+        editable:false,
+        month:minDate.getMonth(),
+        year:minDate.getFullYear(),
+        header:{
+          left: 'today',
+  				center: 'title',
+  				right: 'prev, next'
+        },
+        events: Drupal.settings.basePath + '?q=rooms/units/unit/'
+                                          + Drupal.settings[i].roomsAvailabilityRef.unitID
+                                          + '/availability/json/'
+                                          + Drupal.settings[i].roomsAvailabilityRef.startyear
+                                          + '/'
+                                          + Drupal.settings[i].roomsAvailabilityRef.startmonth
+                                          +'/1/' //start day
+                                          + Drupal.settings[i].roomsAvailabilityRef.endyear
+                                          +'/'
+                                          + Drupal.settings[i].roomsAvailabilityRef.endmonth
+                                          +'/'
+                                          + Drupal.settings[i].roomsAvailabilityRef.endday
+                                          + '/'
+                                          + Drupal.settings[i].roomsAvailabilityRef.style
+      });
+
+      i++;
     });
-    
-    
-    
+
+
     // Resize takes care of some quirks on occasion
     $(window).resize();
 
