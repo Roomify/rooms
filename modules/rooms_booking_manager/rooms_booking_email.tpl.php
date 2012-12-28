32ef51bb2cb08fd7e7d251eeb1d035f99706af8b
Nicolò Caruso <nicolo@istos.it>
2012-12-28 16:26:12 +0100

    Send email on enquiry confirmation

--- modules/rooms_booking_manager/rooms_booking_email.tpl.php
+++ modules/rooms_booking_manager/rooms_booking_email.tpl.php
@@ -1,23 +1,19 @@
-<h1>Booking request</h1>
+<h1><?php print t('Booking request'); ?></h1>
 
-<p>Name: <?php print $customer_name; ?></p>
-<p>Email: <?php print $customer_email; ?></p>
-<p>Address Line 1: <?php print $customer_add1; ?></p>
-<p>Address Line 2: <?php print $customer_add2; ?></p>
-<p>City: <?php print $customer_city; ?></p>
-<p>State/County: <?php print $customer_state; ?></p>
-<p>Country: <?php print $customer_country; ?></p>
-<p>Comments: <?php print $comments; ?></p>
+<p><?php print render($customer_name); ?></p>
+<p><?php print render($customer_email); ?></p>
+<p><?php print render($customer_add1); ?></p>
+<p><?php print render($customer_add2); ?></p>
+<p><?php print render($customer_city); ?></p>
+<p><?php print render($customer_state); ?></p>
+<p><?php print render($customer_country); ?></p>
+<p><?php print render($comments); ?></p>
 
 <p></p>
 
-<h2>Enquiry</h2>
+<h2><?php print t('Enquiry'); ?></h2>
 <?php
-  foreach ($booking_request as $key => $value) {
-  	$temp = explode(':', $key);
-  	$label = db_select('rooms_unit_type', 'n')->fields('n', array('label'))->condition('type', $temp[0], '=')->execute()->fetchField();
-
-  	print '<p>' . $label . ' (';
-  	print $value[0] . ' - ' . $value[1] . ')</p>';
+  foreach ($booking_request as $request) {
+  	print render($request);
 	}
 ?>
