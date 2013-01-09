43196189fc55a4530e46e0e1587abf165bedab9d
Nicolò Caruso <nicolo@istos.it>
2013-01-09 11:58:08 +0100

    ROOMS-24 - Duplicate the place booking button to the bottom of the page

--- modules/rooms_booking_manager/rooms_booking_results.tpl.php
+++ modules/rooms_booking_manager/rooms_booking_results.tpl.php
@@ -5,6 +5,8 @@
 
 <?php if (isset($style) && ($style == ROOMS_INDIVIDUAL)): ?>
   <?php if ($booking_results): ?>
+    <?php print render($change_search); ?>
+    <?php print render($legend); ?>
     <?php foreach ($units_per_type as $unit_type => $units_per_price_level) {
       print render($$unit_type);
       foreach ($units_per_price_level as $price => $units) {
