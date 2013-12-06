cb2542568b325904975aed2ff2022f3dac12287c
James Wilson <jrwilson3@gmail.com>
2013-12-06 01:44:30 -0500

    ROOMS-124: Consistently theme search results for individual rooms.
    Though the search result page does work slightly differently when displaying individual rooms than when choosing from a group of units of the same type, there are lots of elements that could be improved, here are some:
    
    * Add a wrapper div around each type; the class name matches with that used on the per-type table layout.
    * Add a wrapper div around each unit; the class name matches with that used on the per-type table layout.
    * Add classes around each form element.
    * Provide some basic theming and layout so the form doesn’t look horrible out of the box.
    * Use similar terminology (ROOMS-119) to replace the simplistic ‘Base price’  with more realistic phrasing, namely:
      “Book this unit for X nights at Y per person ( Z per person per night)”   <— we can improve upon this from here, if its not clear enough.

--- modules/rooms_booking_manager/rooms_booking_results.tpl.php
+++ modules/rooms_booking_manager/rooms_booking_results.tpl.php
@@ -8,16 +8,24 @@
     <?php print render($legend); ?>
     <?php print render($change_search); ?>
 
-    <?php foreach ($units_per_type as $unit_type => $units_per_price_level) {
-      print render($$unit_type);
-      foreach ($units_per_price_level as $price => $units) {
-        foreach ($units as $unit_id => $unit) {
-          print render($unit['unit']);
-          print render($unit['price']);
-          print render($unit['book_unit_form']);
-        }
-      }
-    }?>
+    <?php foreach ($units_per_type as $type_name => $units_per_price_level): ?>
+      <div class="rooms-search-result__unit-type">
+
+        <?php print render(${$type_name}); ?>
+
+        <?php foreach ($units_per_price_level as $price => $units) : ?>
+          <?php foreach ($units as $unit_id => $unit) : ?>
+            <div class="rooms-search-result__unit-embedded" id="unit_<?php print $unit_id ?>">
+            <?php
+              print render($unit['unit']);
+              print render($unit['price']);
+              print render($unit['book_unit_form']);
+            ?>
+            </div>
+          <?php endforeach; ?>
+        <?php endforeach; ?>
+      </div>
+    <?php endforeach; ?>
   <?php endif; ?>
 <?php endif; ?>
 
