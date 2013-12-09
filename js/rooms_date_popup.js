72f89ca582090a0925348a9544f6edd17ba406e8
James Wilson <jrwilson3@gmail.com>
2013-12-08 19:37:27 -0500

    Issue #2151565 by jwilson3: prevent js datepicker collisions.

--- js/rooms_date_popup.js
+++ js/rooms_date_popup.js
@@ -1,32 +1,55 @@
 (function ($) {
 
-Drupal.behaviors.rooms_date_popup = {
+/*
+ * Default settings for all Rooms datepickers; they come in pairs.
+ */
+Drupal.settings.rooms_datepicker = {
+  // When the startDate is selected, update the minDate attribute of the
+  // endDate picker so users can't pick an inverse date range.
+  onSelect: function(selectedDate) {
+    instance = $(this).data("datepicker");
+    format = instance.settings.dateFormat || $.datepicker._defaults.dateFormat;
+    date = $.datepicker.parseDate(format, selectedDate, instance.settings);
+    if (instance.settings.endDate !== undefined) {
+      instance.settings.endDate.datepicker("option", "minDate", date);
+    }
+  },
+
+  // If you think this is ugly you are right - read this though:
+  // http://blog.foersom.dk/post/598839422/dealing-with-z-index-in-jquery-uis-datepicker
+  beforeShow: function() {
+    setTimeout(function() {
+      $(".ui-datepicker").css("z-index", 12);
+    }, 10);
+  },
+};
+
+/*
+ * Defines the Rooms Date Popup behavior for all start and end date range pairs.
+ */
+Drupal.behaviors.rooms_datepicker = {
   attach: function(context) {
-    var dates = $( "#edit-rooms-start-date-datepicker-popup-0, #edit-rooms-end-date-datepicker-popup-0" ).datepicker({
-    dateFormat: Drupal.settings.rooms.roomsDateFormat,
-      //defaultDate: "+1w",
-      minDate: "+" + Drupal.settings.rooms.roomsBookingStartDay + "d",
-      //maxDate: "+2M +10D", // This can be set via a variable to somthing reasonable
-      //changeMonth: true,
-      //numberOfMonths: 1,
-      onSelect: function( selectedDate ) {
-        var option = this.id == "edit-rooms-start-date-datepicker-popup-0" ? "minDate" : "na", //can change na to maxDate to set the range
-          instance = $( this ).data( "datepicker" ),
-          date = $.datepicker.parseDate(
-            instance.settings.dateFormat ||
-            $.datepicker._defaults.dateFormat,
-            selectedDate, instance.settings );
-        dates.not( this ).datepicker( "option", option, date );
-      },
-      beforeShow: function()
-      {
-        setTimeout(function()
-          {
-            // If you think this is ugly you are right - read this though: http://blog.foersom.dk/post/598839422/dealing-with-z-index-in-jquery-uis-datepicker
-            $(".ui-datepicker").css("z-index", 12);
-          }, 10);
-      }
-    });
+    // Iterate through pairs of date fields (there may be multiple ones on
+    // a page) and setup the jQuery datepickers combining default settings
+    // with any custom settings provided. Date ranges pairs are defined by
+    // the Drupal.settings.rooms.datepickers object, keyed by the unique id
+    // of the startDate wrapper element.
+    for (var startDateId in Drupal.settings.rooms.datepickers) {
+      var endDateId = Drupal.settings.rooms.datepickers[startDateId].endDateId;
+      var endDate = $("#" + endDateId + ' .form-text').datepicker(
+        $.extend(
+          Drupal.settings.rooms_datepicker, // Defaults defined above.
+          Drupal.settings.rooms.datepickers[startDateId].settings // Settings from Drupal.
+        )
+      );
+      var startDate = $("#" + startDateId+ ' .form-text').datepicker(
+        $.extend(
+          { 'endDate': endDate }, // Reference to endDate picker.
+          Drupal.settings.rooms_datepicker, // Defaults.
+          Drupal.settings.rooms.datepickers[startDateId].settings // Settings from Drupal.
+        )
+      );
+    }
   }
 };
 })(jQuery);
