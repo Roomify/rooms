e4d86acb3afe97d61f4b40852e9b289a39cdda90
James Wilson <jrwilson3@gmail.com>
2014-06-09 14:56:22 -0500

    ROOMS-278: Extract rooms option widget styles into a separate stylesheet.

--- js/rooms_options_widget.js
+++ js/rooms_options_widget.js
@@ -2,13 +2,13 @@
   /**
    * Booking unit options widget JS operations.
    */
-  Drupal.behaviors.rooms_operations_widget = {
+  Drupal.behaviors.rooms_options_widget = {
     attach: function(context) {
-      $('.remove-booking-option').click(function(e) {
-        $(this).parent().find('.booking-option-name').val('');
+      $('.rooms-option--remove-button').click(function(e) {
+        $(this).parent().find('.rooms-option--name').val('');
         $(this).parents('tr').hide();
         e.preventDefault();
-      })
+      });
     }
   };
 })(jQuery);
