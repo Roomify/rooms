9eb27fd0642d8281f7e096bf1e4f9f39c2501332
ronald_istos <ronald@istos.it>
2015-02-23 17:35:14 +0100

    Additional fixes and updates to color choices

--- modules/rooms_availability/js/rooms_unit_management.js
+++ modules/rooms_availability/js/rooms_unit_management.js
@@ -70,6 +70,9 @@ Drupal.behaviors.roomsAvailability = {
           right: ''
         },
         events: Drupal.settings.basePath + '?q=rooms/units/unit/' + Drupal.settings.roomsUnitManagement.roomsId[c] + '/availability/json/' + value[2] + '/' + phpmonth,
+        windowResize: function(view) {
+          $(value[0]).fullCalendar('refetchEvents');
+        },
         eventClick: function(calEvent, jsEvent, view) {
           // Getting the Unix timestamp - JS will only give us milliseconds
           if (calEvent.end === null) {
@@ -89,11 +92,51 @@ Drupal.behaviors.roomsAvailability = {
           Drupal.RoomsAvailability.Modal(this, unit_id, -2, sd, ed);
           $(value[0]).fullCalendar('unselect');
         },
-        //Remove Time from events
-        eventRender: function(event, el) {
-          el.find('.fc-time').remove();
-        }
 
+        eventRender: function(event, el) {
+          //Remove Time from events
+          el.find('.fc-time').remove();
+          // Add a class if the event start it is not "AV" or "N/A".
+          if (el.hasClass('fc-start') && this.id != 1 && this.id != 0) {
+            el.append('<div class="event-start"/>');
+            el.find('.event-start').css('border-top-color', this.color);
+          }
+
+          // Add a class if the event end and it is not "AV" or "N/A".
+          if (el.hasClass('fc-end') && this.id != 1 && this.id != 0) {
+            el.append('<div class="event-end"/>');
+            el.find('.event-end').css('border-top-color', this.color);
+          }
+        },
+        eventAfterRender: function( event, element, view ) {
+          // Event width.
+          var width = element.parent().width()
+          // Event colspan number.
+          var colspan = element.parent().get(0).colSpan;
+          // Single cell width.
+          var cell_width = width/colspan;
+          var half_cell_width = cell_width/2;
+
+          // Move events between table margins.
+          element.css('margin-left', half_cell_width);
+          element.css('margin-right', half_cell_width);
+
+          // Calculate width event to add end date triangle.
+          width_event = element.children('.fc-content').width();
+          console.log('Width_event',width_event);
+          // Add a margin left to the top triangle.
+          element.children().closest('.event-end').css('margin-left', width_event-17);
+
+          // If the event end in a next row.
+          if(element.hasClass('fc-not-end')) {
+            element.css('margin-right', 0);
+          }
+          // If the event start in a previous row.
+          if(element.hasClass('fc-not-start')) {
+            element.css('margin-left', 0);
+            element.children().closest('.event-end').css('margin-left', width_event);
+          }
+        }
       });
 
       c++;
