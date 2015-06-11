2e2b8cf3a45c5ace9e0b38121df179437d5250c3
Salvo Scala <salvoscala@gmail.com>
2015-06-11 12:17:35 +0200

    TOUR-12: Create Rooms Configuration that reverts to old calendar display type

---
+++ modules/rooms_availability/js/rooms_availability_full_day.js
@@ -0,0 +1,132 @@
+(function ($) {
+// define object
+Drupal.RoomsAvailability = Drupal.RoomsAvailability || {};
+Drupal.RoomsAvailability.Modal = Drupal.RoomsAvailability.Modal || {};
+Drupal.behaviors.rooms_availability = {
+  attach: function(context) {
+
+    // Current month is whatever comes through -1 since js counts months starting from 0
+    currentMonth = Drupal.settings.roomsCalendar.currentMonth - 1;
+    currentYear = Drupal.settings.roomsCalendar.currentYear;
+    firstDay = Drupal.settings.roomsCalendar.firstDay;
+
+    // The first month on the calendar
+    month1 = currentMonth;
+    year1 = currentYear;
+
+    // Second month is the next one obviously unless it is 11 in which case we need to move a year ahead
+    if (currentMonth == 11) {
+      month2 = 0;
+      year2 = year1 + 1;
+    }
+    else{
+      month2 = currentMonth+1;
+      year2 = currentYear;
+    }
+
+    currentMonth = month2;
+    // And finally the last month where we do the same as above worth streamlining this probably
+    if (currentMonth == 11) {
+      month3 = 0;
+      year3 = year2 + 1;
+    }
+    else{
+      month3 = currentMonth+1;
+      year3 = year2;
+    }
+
+    var calendars = [];
+    calendars[0] = new Array('#calendar', month1, year1);
+    calendars[1] = new Array('#calendar1', month2, year2);
+    calendars[2] = new Array('#calendar2', month3, year3);
+
+    // refresh the events once the modal is closed
+    $("#modalContent a.close").once().bind('click', function(e) {
+      $.each(calendars, function(key, value) {
+        $(value[0]).fullCalendar('refetchEvents');
+      });
+    });
+
+    $.each(calendars, function(key, value) {
+      // phpmonth is what we send via the url and need to add one since php handles
+      // months starting from 1 not zero
+      phpmonth = value[1]+1;
+      $(value[0]).once().fullCalendar({
+        ignoreTimezone: false,
+        editable: false,
+        selectable: true,
+        handleWindowResize: true,
+        height: 400,
+        dayNamesShort:[Drupal.t("Sun"), Drupal.t("Mon"), Drupal.t("Tue"), Drupal.t("Wed"), Drupal.t("Thu"), Drupal.t("Fri"), Drupal.t("Sat")],
+        monthNames:[Drupal.t("January"), Drupal.t("February"), Drupal.t("March"), Drupal.t("April"), Drupal.t("May"), Drupal.t("June"), Drupal.t("July"), Drupal.t("August"), Drupal.t("September"), Drupal.t("October"), Drupal.t("November"), Drupal.t("December")],
+        firstDay: firstDay,
+        defaultDate: moment([value[2],phpmonth-1]),
+        header:{
+          left: 'title',
+          center: '',
+          right: '',
+        },
+        windowResize: function(view) {
+          $(value[0]).fullCalendar('refetchEvents');
+        },
+        events: Drupal.settings.basePath + '?q=rooms/units/unit/' + Drupal.settings.roomsAvailability.roomID + '/availability/json/' + value[2] + '/' + phpmonth,
+        eventClick: function(calEvent, jsEvent, view) {
+          // Getting the Unix timestamp - JS will only give us milliseconds
+          if (calEvent.end === null) {
+            //We are probably dealing with a single day event
+            calEvent.end = calEvent.start;
+          }
+          var sd = calEvent.start.unix();
+          var ed = calEvent.end.unix();
+
+          // Open the modal for edit
+          Drupal.RoomsAvailability.Modal(view, calEvent.id, sd, ed);
+        },
+        select: function(start, end, allDay) {
+          var ed = end.subtract(1, 'days');
+          var sd = start.unix();
+          ed = end.unix();
+          // Open the modal for edit
+          Drupal.RoomsAvailability.Modal(this, -2, sd, ed);
+          $(value[0]).fullCalendar('unselect');
+        },
+        eventRender: function(event, el) {
+          // Remove Time from events.
+          el.find('.fc-time').remove();
+        },
+      });
+    });
+  }
+};
+
+/**
+* Initialize the modal box.
+*/
+Drupal.RoomsAvailability.Modal = function(element, eid, sd, ed) {
+  // prepare the modal show with the rooms-availability settings.
+  Drupal.CTools.Modal.show('rooms-modal-style');
+  // base url the part that never change is used to identify our ajax instance
+  var base = Drupal.settings.basePath + '?q=admin/rooms/units/unit/';
+  // Create a drupal ajax object that points to the rooms availability form.
+  var element_settings = {
+    url : base + Drupal.settings.roomsAvailability.roomID + '/event/' + eid + '/' + sd + '/' + ed,
+    event : 'getResponse',
+    progress : { type: 'throbber' }
+  };
+  // To made all calendars trigger correctly the getResponse event we need to
+  // initialize the ajax instance with the global calendar table element.
+  var calendars_table = $(element.el).closest('.calendar-set');
+  // create new instance only once if exists just override the url
+  if (Drupal.ajax[base] === undefined) {
+    Drupal.ajax[base] = new Drupal.ajax(element_settings.url, calendars_table, element_settings);
+  }
+  else {
+    Drupal.ajax[base].element_settings.url = element_settings.url;
+    Drupal.ajax[base].options.url = element_settings.url;
+  }
+  // We need to trigger manually the AJAX getResponse due fullcalendar select
+  // event is not recognized by Drupal AJAX
+  $(calendars_table).trigger('getResponse');
+};
+
+})(jQuery);
---
+++ modules/rooms_availability/js/rooms_unit_management_full_day.js
@@ -0,0 +1,141 @@
+(function ($) {
+// define object
+Drupal.RoomsAvailability = Drupal.RoomsAvailability || {};
+Drupal.RoomsAvailability.Modal = Drupal.RoomsAvailability.Modal || {};
+
+Drupal.behaviors.roomsAvailabilityPrepareForm = {
+  attach: function(context) {
+    $("form#rooms-filter-month-form select").once('select').change(function() {
+      $("form#rooms-filter-month-form").submit();
+    });
+
+    $('#edit-select-all').once('select').change(function() {
+      var table = $(this).closest('table')[0];
+      if (this.options.selectedIndex == 1) {
+        $('input[id^="edit-rooms"]:not(:disabled)', table).attr('checked', true);
+      }
+      else if (this.options.selectedIndex == 2) {
+        $('input[id^="edit-rooms"]:not(:disabled)', table).attr('checked', true);
+      }
+      else if (this.options.selectedIndex == 3) {
+        $('input[id^="edit-rooms"]:not(:disabled)', table).attr('checked', false);
+      }
+    });
+  }
+};
+
+Drupal.behaviors.roomsAvailability = {
+  attach: function(context) {
+
+    // Current month is whatever comes through -1 since js counts months starting from 0
+    currentMonth = parseInt(Drupal.settings.roomsUnitManagement.currentMonth)-1;
+    currentYear = parseInt(Drupal.settings.roomsUnitManagement.currentYear);
+
+    // The first month on the calendar
+    month1 = currentMonth;
+    year1 = currentYear;
+
+    var calendars = [];
+    var i = 0;
+    for (i=0;i<Drupal.settings.roomsUnitManagement.roomsNumber;i++) {
+      calendars[i] = new Array('#calendar' + i, month1, year1);
+    }
+
+    // refresh the events once the modal is closed
+    $("#modalContent a.close").once().bind('click', function(e) {
+      $.each(calendars, function(key, value) {
+        $(value[0]).fullCalendar('refetchEvents');
+      });
+    });
+
+    var c = 0;
+    $.each(calendars, function(key, value) {
+      // phpmonth is what we send via the url and need to add one since php handles
+      // months starting from 1 not zero
+      phpmonth = value[1]+1;
+
+      var unit_id = Drupal.settings.roomsUnitManagement.roomsId[c];
+
+      $(value[0]).once().fullCalendar({
+        ignoreTimezone:false,
+        editable:false,
+        selectable: true,
+        dayNamesShort:[Drupal.t("Sun"), Drupal.t("Mon"), Drupal.t("Tue"), Drupal.t("Wed"), Drupal.t("Thu"), Drupal.t("Fri"), Drupal.t("Sat")],
+        monthNames:[Drupal.t("January"), Drupal.t("February"), Drupal.t("March"), Drupal.t("April"), Drupal.t("May"), Drupal.t("June"), Drupal.t("July"), Drupal.t("August"), Drupal.t("September"), Drupal.t("October"), Drupal.t("November"), Drupal.t("December")],
+        defaultView:'singleRowMonth',
+        defaultDate: moment([value[2],phpmonth-1]),
+        header:{
+          left: 'title',
+          center: '',
+          right: ''
+        },
+        events: Drupal.settings.basePath + '?q=rooms/units/unit/' + Drupal.settings.roomsUnitManagement.roomsId[c] + '/availability/json/' + value[2] + '/' + phpmonth,
+        windowResize: function(view) {
+          $(value[0]).fullCalendar('refetchEvents');
+        },
+        eventClick: function(calEvent, jsEvent, view) {
+          // Getting the Unix timestamp - JS will only give us milliseconds
+          if (calEvent.end === null) {
+            //We are probably dealing with a single day event
+            calEvent.end = calEvent.start;
+          }
+          var sd = calEvent.start.unix();
+          var ed = calEvent.end.unix();
+          // Open the modal for edit
+          Drupal.RoomsAvailability.Modal(view, unit_id, calEvent.id, sd, ed);
+        },
+        select: function(start, end, allDay) {
+          var ed = end.subtract(1, 'days');
+          var sd = start.unix();
+          var ed = end.unix();
+          // Open the modal for edit
+          Drupal.RoomsAvailability.Modal(this, unit_id, -2, sd, ed);
+          $(value[0]).fullCalendar('unselect');
+        },
+
+        eventRender: function(event, el) {
+          //Remove Time from events
+          el.find('.fc-time').remove();
+        },
+      });
+
+      c++;
+    });
+
+    // Resize takes care of some quirks on occasion
+    $(window).resize();
+
+  }
+};
+
+/**
+* Initialize the modal box.
+*/
+Drupal.RoomsAvailability.Modal = function(element, unit_id, eid, sd, ed) {
+  // prepare the modal show with the rooms-availability settings.
+  Drupal.CTools.Modal.show('rooms-modal-style');
+  // base url the part that never change is used to identify our ajax instance
+  var base = Drupal.settings.basePath + '?q=admin/rooms/units/unit/';
+  // Create a drupal ajax object that points to the rooms availability form.
+  var element_settings = {
+    url : base + unit_id + '/event/' + eid + '/' + sd + '/' + ed,
+    event : 'getResponse',
+    progress : { type: 'throbber' },
+  };
+  // To made all calendars trigger correctly the getResponse event we need to
+  // initialize the ajax instance with the global calendar table element.
+  var calendars_table = $(element.el).closest('table');
+  // create new instance only once if exists just override the url
+  if (Drupal.ajax[base] === undefined) {
+    Drupal.ajax[base] = new Drupal.ajax(element_settings.url, calendars_table, element_settings);
+  }
+  else {
+    Drupal.ajax[base].element_settings.url = element_settings.url;
+    Drupal.ajax[base].options.url = element_settings.url;
+  }
+  // We need to trigger manually the AJAX getResponse due fullcalendar select
+  // event is not recognized by Drupal AJAX
+  $(calendars_table).trigger('getResponse');
+};
+
+})(jQuery);
