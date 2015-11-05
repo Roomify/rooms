6c15643cbc475dfb357d0346df04aad2638b5c95
Achton <achton@netherclift.net>
2015-11-05 16:34:47 +0100

    Let referenced calendars use Drupal's locale setting for first day of week.

--- modules/rooms_availability_reference/js/rooms_availability_reference.js
+++ modules/rooms_availability_reference/js/rooms_availability_reference.js
@@ -14,6 +14,7 @@
           dayNamesShort:[Drupal.t("Sun"), Drupal.t("Mon"), Drupal.t("Tue"), Drupal.t("Wed"), Drupal.t("Thu"), Drupal.t("Fri"), Drupal.t("Sat")],
           monthNames:[Drupal.t("January"), Drupal.t("February"), Drupal.t("March"), Drupal.t("April"), Drupal.t("May"), Drupal.t("June"), Drupal.t("July"), Drupal.t("August"), Drupal.t("September"), Drupal.t("October"), Drupal.t("November"), Drupal.t("December")],
           defaultDate: today,
+          firstDay: Drupal.settings.roomsCalendar.firstDay,
           header:{
             left: 'today',
             center: 'title',
