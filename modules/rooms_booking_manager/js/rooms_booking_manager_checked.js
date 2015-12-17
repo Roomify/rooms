a78a61a1aeb135ab27380f679ba46170563cc051
istos <ronald@roomify.us>
2015-12-17 11:14:50 +0100

    RD-1: Clean up Rooms 2.x (will be working with BAT)

--- modules/rooms_booking_manager/js/rooms_booking_manager_checked.js
+++
@@ -1,8 +0,0 @@
-(function ($) {
-  $(document).ready(function() {
-    $('#edit-perc-textfield').focus(function()
-    {
-      $('input[name=rooms_payment_options][value=11]').attr('checked', true);
-    });
-  });
-})(jQuery);
--- modules/rooms_booking_manager/rooms_booking_override_confirmation_page.tpl.php
+++
@@ -1,2 +0,0 @@
-<?php print render($change_search); ?>
-<?php print render($confirm_form); ?>
